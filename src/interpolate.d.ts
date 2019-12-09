b929af7f3b99d04b09fc5819537c46438562919a
Evan Plaice <evanplaice@gmail.com>
2019-12-09 01:16:20 -0700

    Add Typings

---
+++ src/interpolate.d.ts
@@ -0,0 +1,8 @@
+/**
+ * Interpolate a tagged template literal from the inputs
+ *
+ * @param {*} template the template literal string
+ * @param {*} [tags] the tagged values in the template
+ * @returns the template output with the tagged literals applied
+ */
+export default function interpolate(template: any, tags?: any): any;
