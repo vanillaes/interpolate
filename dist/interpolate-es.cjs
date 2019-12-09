4fd982d9b56447567735bf624d1de49e3cafb5d2
Evan Plaice <evanplaice@gmail.com>
2019-12-09 12:17:20 -0700

    Add CommonJS Build

---
+++ dist/interpolate-es.cjs
@@ -0,0 +1,17 @@
+'use strict';
+
+/* eslint-disable no-new-func */
+/**
+ * Interpolate a tagged template literal from the inputs
+ *
+ * @param {*} template the template literal string
+ * @param {*} [tags] the tagged values in the template
+ * @returns the template output with the tagged literals applied
+ */
+function interpolate (template, tags = {}) {
+  const keys = Object.keys(tags);
+  const values = Object.values(tags);
+  return new Function(...keys, `return \`${template}\`;`)(...values);
+}
+
+module.exports = interpolate;
