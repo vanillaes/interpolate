91e6de4f15dad49aa2de3765c1b4a982a827f8dc
Evan Plaice <evanplaice@gmail.com>
2021-05-03 11:51:06 -0600

    Build/Minify with ESMTK

--- index.cjs
+++ index.cjs
@@ -1,40 +1,34 @@
-'use strict';
+var __defProp = Object.defineProperty;
+var __markAsModule = (target) => __defProp(target, "__esModule", {value: true});
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, {get: all[name], enumerable: true});
+};
 
-Object.defineProperty(exports, '__esModule', { value: true });
-
-/* eslint-disable no-new-func */
-/**
- * Interpolate a tagged template literal from the inputs
- *
- * @param {*} template the template literal string
- * @param {*} [tags] the tagged values in the template
- * @returns the template output with the tagged literals applied
- */
-function interpolate (template, tags = {}) {
+// index.js
+__markAsModule(exports);
+__export(exports, {
+  interpolate: () => interpolate
+});
+function interpolate(template, tags = {}) {
   const keys = Object.keys(tags);
   const values = Object.values(tags);
   try {
-    return new Function(...keys, `return \`${template}\`;`)(...values)
+    return new Function(...keys, `return \`${template}\`;`)(...values);
   } catch (e) {
-    throw new TemplateException(template, tags, e)
+    throw new TemplateException(template, tags, e);
   }
 }
-
-/**
- * @private
- */
-class TemplateException extends Error {
-  constructor (template, tags, message) {
+var TemplateException = class extends Error {
+  constructor(template, tags, message) {
     super();
-    this.name = 'TemplateError';
-    let msg = '\n------------------\n';
+    this.name = "TemplateError";
+    let msg = "\n------------------\n";
     msg += `Template: \`${template}\``;
-    msg += '\n------------------\n';
+    msg += "\n------------------\n";
     msg += `Tags: ${JSON.stringify(tags, null, 2)}`;
-    msg += '\n------------------\n';
+    msg += "\n------------------\n";
     msg += message;
     this.message = msg;
   }
-}
-
-exports.interpolate = interpolate;
+};
